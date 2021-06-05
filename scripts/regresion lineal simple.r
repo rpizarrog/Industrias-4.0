ccce8fa68a657e89e8ff64bf21ee9677fd403030
rpizarrog <rpizarro@itdurango.edu.mx>
2021-06-05 13:42:28 -0500

    Create regresion lineal simple.r

---
+++ scripts/regresion lineal simple.r
@@ -0,0 +1,58 @@
+# Regresión lineal simple
+# Para predecir la cantidad de achaques de una persona 
+# conforme a la edad
+
+
+# Librerías
+# install.packages("ggplot2")
+library(ggplot2) # Gráficos amigables
+
+# <- es igua que el signo de igual <- es = como asignación
+
+edades <- c(10, 15, 20, 30, 40, 56, 67, 82, 45, 46, 58, 66, 70, 19)
+achaques <- c(5, 7, 8, 10, 15, 20, 22, 16, 28, 26, 18, 23, 24, 22) # escala 5-30
+
+
+edades
+achaques
+
+
+datos <- data.frame(edades, achaques)
+datos
+
+
+
+# Gráfica de dispersión 
+ggplot(data = datos, mapping = aes(x = edades, y = achaques)) +
+  geom_point(colour = "blue")
+
+
+
+# Construir un modelo  de Regresión Lineal Simple
+modelo <- lm(data = datos, formula = achaques ~ edades)
+sm <- summary(modelo) # El resumen de los estadísticos
+
+sm 
+
+
+
+ggplot(data = datos, aes(x = edades, y = achaques)) +
+  geom_point(colour = "blue") +
+  geom_line(aes(x=edades, y = modelo$fitted.values), color = 'red')
+  
+  
+
+# nuevos datos de edades
+
+nuevas.edades <- c(55, 60, 20)
+
+
+# Predecir las edades de 55, 60 y 20 para las personas nuevas
+predict(object = modelo, newdata = data.frame(edades = nuevas.edades))
+
+
+        
+
+
+
+
